b5bed0d79735d0b047a2745d074fb5aec4c67ab2
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-02-24 14:59:35 +0200

    comit 14/59

---
+++ js/lesson-01/modul-1.js
@@ -0,0 +1,12 @@
+// console.log("Hello, world");
+
+// alert("Hello");
+
+// const userName = prompt("Введіть ім'я");
+// console.log(userName);
+
+// const num = 10;
+// console.log(num);
+
+// num = 20;
+// console.log(num);
