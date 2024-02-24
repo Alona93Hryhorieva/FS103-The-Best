c5cd306f58bf553346c60a47a615be297dc6a363
Alina Prystynska <pristinskayaalina9@gmail.com>
2024-02-24 13:51:00 +0000

    15-50

--- js/lesson-01/modul-1.js
+++ js/lesson-01/modul-1.js
@@ -10,3 +10,37 @@
 
 // num = 20;
 // console.log(num);
+
+
+/*
+const $ = 10;
+const $num = 20;
+const num$ = 30;
+const _ = 40;
+const num10 = 10;
+const num_ = -20;
+const first_name = "kiwi"; //worse
+ */
+
+
+const firstName = "orange"; //good
+
+// const 5num =10;
+// const first-name ="mango";
+// const name = "Kolya";
+// const let = "hello";
+// const const = "world";
+// const false = false;
+
+/*
+const num =10;
+const string ="hello";
+const bool = true;
+const sameDate = null;
+const anotheDate= undefined;
+
+console.log(typeof num);
+console.log(typeof string);
+console.log(typeof bool);
+console.log(typeof sameDate);
+console.log(typeof anotheDate);*/
