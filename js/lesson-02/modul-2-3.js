70a7ecfc7c1af8d7e930d0099f88adbfa36a4d54
marinaserdyukz <serdyukmarina876@gmail.com>
2024-03-09 13:37:46 +0100

    if else

---
+++ js/lesson-02/modul-2-3.js
@@ -0,0 +1,22 @@
+//* Розгалуження: if, if...else, else...if
+//TODO: 1 ===================================
+//Даний рядок, що складається із символів, наприклад, 'abcde'.
+// Перевірте, що першим символом цього рядка є буква 'a'.
+// Якщо це так - виведіть 'так', інакше виведіть 'ні'.
+
+//const str = "abcde";
+// if (str.startsWith("b")) {
+// console.log("так");
+// } else {
+// console.log("ні");
+//}
+
+//function checkString(string) {
+//if (str.startsWith("b")) {
+//return "так";
+//} else {
+//return "ні";
+//}
+//}
+
+//console.log(checkString(str));
